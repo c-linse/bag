d07ac75daa9ad1bc518728b99afe06af9453ab02
Your Name <you@example.com>
2025-09-22 13:11:54 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
+    base: '/bag/', // <-- repo name with leading/trailing slashes
+    optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
